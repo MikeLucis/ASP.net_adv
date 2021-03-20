55827a2e4570c23df02278b8e79789cfd4fb7dd2
Mikelucis <liushijie2014@hotmail.com>
2021-03-20 14:16:43 +0800

    ChangeClass; FinishUpdate;

--- ASP.NET_src/ASP.NET_src/Controllers/HomeController.cs
+++ ASP.NET_src/ASP.NET_src/Controllers/HomeController.cs
@@ -10,8 +10,19 @@ using ASP.NET_src.Models;
 
 namespace ASP.NET_src.Controllers
 {
+    //实现验证控件-TODO
+//    [HttpPost]
+//    public ActionResult Edit(User user)
+//    {
+//        if (ModelState.)
+//        {
+//            return Content
+//        }
+//    }
+
     public class HomeController : Controller
     {
+        //Home默认页视图函数
         [HttpGet]
         public ActionResult Index()
         {
@@ -21,17 +32,27 @@ namespace ASP.NET_src.Controllers
         [HttpPost]
         public ActionResult Index(string username, string password)
         {
-            if(username=="abc" && password=="123")
+            //使用@foreach-TODO
+            if (username == "" && password == "")
             {
-                ViewBag.msg = ("Finish!!");
+                ViewBag.msg = false;
             }
             else
             {
-                ViewBag.msg = ("Error!");
+                if (username == "abc" && password == "123")
+                {
+                    ViewBag.msg = ("Finish!!");
+                }
+                else
+                {
+                    ViewBag.msg = ("Error!");
+                } 
             }
             return View();
         }
 
+
+        //Home注册页视图函数
         [HttpGet]
         public ActionResult Register()
         {
@@ -39,12 +60,12 @@ namespace ASP.NET_src.Controllers
         }
 
         [HttpPost]
-        public ActionResult Register(RegisterInfo reg)
+        public ActionResult Register(User user)
         {
             using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["SQL"].ConnectionString))
             {
-                string cmd = "Insert into Register(UserName, Email, PhoneNum, Password1) Values(@UserName, @Email, @PhoneNum, @Password1)";
-                int i = con.Execute(cmd, reg);
+                string cmd = "Insert into Register(UserName, Email, PhoneNum, ConfimPassWords) Values(@UserName, @Email, @PhoneNum, @ConfimPassWords)";
+                int i = con.Execute(cmd, user);
                 if (i > 0)
                 {
                     ViewBag.msg = "Finish !!";
@@ -57,6 +78,42 @@ namespace ASP.NET_src.Controllers
                 
             }
                 return View();
+        }
+
+
+        //Home更改页视图函数
+        [HttpGet]
+        public ActionResult Update(int ID)
+        {
+            var con = new SqlConnection(ConfigurationManager.ConnectionStrings["SQL"].ConnectionString);
+
+            string cmd = $"Select * From dbo.Register Where ID = {ID}";
+
+            var info = con.Query<User>(cmd).SingleOrDefault();
+
+            ViewData["UserInfo"] = info;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Update(int ID, User reg)
+        {
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["SQL"].ConnectionString))
+            {
+                string cmd = $"Update dbo.Register SET UserName = @UserName, Email = @Email, PhoneNum = @PhoneNum, ConfimPassWords = @ConfimPassWords Where (ID = {ID})";
+                int i = con.Execute(cmd, reg);
+                if (i > 0)
+                {
+                    ViewBag.msg = "Finish !!";
+                }
+                else
+                {
+                    ViewBag.msg = "Error !!";
+                }
+
+            }
+            return View("UpdateResult");
         }
     }
 }
---
+++ ASP.NET_src/ASP.NET_src/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_src.Models
+{
+    public class User
+    {
+        //[Required(ErrorMessage = "请输入收件人姓名")]
+        //[Display(Name = "用户名")]
+        public string UserName { get; set; }
+
+        //[Required(ErrorMessage = "邮箱为必填项")]
+        //[RegularExpression(@"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$", ErrorMessage = "请输入正确的电子邮箱地址")]
+        public string Email { get; set; }
+
+        //[Required(ErrorMessage = "请输入收货人手机号")]
+        //[RegularExpression(@"^1[3458][0-9]{9}$", ErrorMessage = "手机号格式不正确")]
+        // 允许空值
+        public string PhoneNum { get; set; }
+
+        //[StringLength(16, MinimumLength = 6, ErrorMessage = "密码长度应在6~16位之间")]
+        //[System.ComponentModel.DataAnnotations.Compare("ConfimPassWords")]
+        ////二次输入密码必须和上次相同
+        //public string NewPassWords { get; set; }
+
+        //[DataType(DataType.Password)]
+        //[StringLength(16, MinimumLength = 6, ErrorMessage = "密码长度应在6~16位之间")]
+        //[System.ComponentModel.DataAnnotations.Compare("NewPassWords")]
+        //二次输入密码必须和上次相同
+        public string ConfimPassWords { get; set; }//密码长度 6~16位 
+    }
+}
